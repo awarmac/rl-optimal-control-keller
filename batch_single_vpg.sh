521e9b3641dbab8f1af67027f6e5cc1b4d7237c8
Sajad <sajad.s.shahsavari@utu.fi>
2022-01-03 13:56:27 +0200

    add batch as argument and setup log_every in bash

--- batch_single_vpg.sh
+++ batch_single_vpg.sh
@@ -11,22 +11,25 @@
 module load pytorch/1.10 gcc/9.3.0 openmpi/4.0.3
 #source fenv/bin/activate
 
-if [ $# -ne 7 ]; then
-  echo "batch_single_vpg.sh <experiment-number> <experiment-info> <hidden-sizes> <lr> <epochs> <gaussian-log-std> <track-length>"
-  echo " e.g batch_single_vpg.sh 9 smallcapacity-longtrack 32,32 0.001 500 -0.5 1000"
+if [ $# -ne 8 ]; then
+  echo "batch_single_vpg.sh <experiment-number> <experiment-info> <hidden-sizes> <lr> <epochs> <gaussian-log-std> <track-length> <batch-size>"
+  echo " e.g batch_single_vpg.sh 9 smallcapacity-longtrack 32,32 0.001 500 -0.5 1000 5000"
   exit 1;
 fi
 
 base_logdir="/scratch/project_2005209/velcontrol-rl-keller/runvpg/"
 mkdir -p $base_logdir
+
 exp_num=$1
 exp_info=$2
 hidden_sizes=$3
 lr=$4
 epochs=$5
-batch_size=5000
 gaussian_log_std=$6
 track_length=$7
+batch_size=$8
+
+log_every=50 #episodes
 
 exp_name=exp-$exp_num-$exp_info
 mkdir -p $base_logdir/$exp_name
@@ -36,7 +39,7 @@ echo -e "hidden_sizes:$hidden_sizes \nlr: $lr \nepochs $epochs \
 
 srun python vpg_train.py --epochs $epochs --batch-size $batch_size --lr $lr --hidden-sizes $hidden_sizes \
 				--gaussian-log-std "$gaussian_log_std" --track-length $track_length \
-				--log-dir $base_logdir --exp-name $exp_name --log-raw-csv --log-raw-csv-every 200 >> $base_logdir/log-${exp_name}.txt 2>&1
+				--log-dir $base_logdir --exp-name $exp_name --log-raw-csv --log-raw-csv-every $log_every >> $base_logdir/log-${exp_name}.txt 2>&1
 python vpg_plot_train_history.py $base_logdir/$exp_name/${exp_name}_s0
 python vpg_run_model.py $base_logdir/$exp_name/${exp_name}_s0/pyt_save/model.pt --num-episodes 10
 
